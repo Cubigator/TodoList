6519a6250342958ab7b09c96a66b2f1b63935a74
Sergey-piksel <sergeychuprin2006@gmail.com>
2022-11-12 17:40:53 +0700

    Добавление новой задачи. Сохранение всех данных в json

--- AddExercise.xaml.cs
+++ AddExercise.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace TodoList.ViewModels
+namespace TodoList
 {
     /// <summary>
     /// Логика взаимодействия для AddExercise.xaml
--- ViewModels/MainVM.cs
+++ ViewModels/MainVM.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using TodoList.Model;
 using TodoList.Savings;
 
@@ -16,29 +17,87 @@ namespace TodoList.ViewModels
         public MainVM()
         {
             _AllExercises = JsonHandler.Load();
+            UpdateCategories();
+            _addedTask = new Exercise();
+            Selected = new Exercise();
+        }
+
+        void UpdateCategories()
+        {
             Todos = (from ex in _AllExercises where ex.IsDone == false select ex).ToList();
             Finished = (from ex in _AllExercises where ex.IsDone == true select ex).ToList();
         }
+
+        static Exercise _addedTask;
+        public Exercise Selected { get; set; }
+
+        static AddExercise _addExercise = new AddExercise();
+        public Exercise AddedTask
+        {
+            get { return _addedTask; }
+            set
+            {
+                _addedTask = value;
+                Notify("AddedTask");
+            }
+        }
+
+
         List<Exercise> _AllExercises;
         public List<Exercise> Todos { get; set; }
         public List<Exercise> Finished { get; set; }
+
 
         void Notify(string name)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
-        public ButtonCommand AddTodos
+        public ButtonCommand AddTodosMenu
         {
             get
             {
                 return new ButtonCommand(() =>
                     {
-                        AddExercise addExercise = new AddExercise();
-                        addExercise.ShowDialog();
+                        _addExercise.ShowDialog();
+                        if(_addExercise.DialogResult == true)
+                        {
+                            _AllExercises.Add(AddedTask);
+                            Todos.Add(AddedTask);
+                            Todos = new List<Exercise>(Todos);
+                            Notify("Todos");
+                            Notify("Finished");
+                        }
+                        _addExercise = new AddExercise();
+                        JsonHandler.Save(_AllExercises);
                     });
             }
         }
 
+        public ButtonCommand CloseAddWindow
+        {
+            get
+            {
+                return new ButtonCommand(() =>
+                {
+                    _addExercise.DialogResult = true;
+                });
+            }
+        }
+
+        public ButtonCommand DeleteExercise
+        {
+            get
+            {
+                return new ButtonCommand(() =>
+                {
+                    _AllExercises.Remove(Selected);
+                    UpdateCategories();
+                    Notify("Todos");
+                    Notify("Finished");
+                    JsonHandler.Save(_AllExercises);
+                });
+            }
+        }
     }
 }
